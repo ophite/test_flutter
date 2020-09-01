b9d9fbbb5185d5d7b3a64359aa9d6cb8bdbae094
yurakobernik <yurii.k@maxitechs.com>
2020-09-02 00:12:03 +0300

    setup firebase to android

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times33344ss:',
+              'You have pushed the button this many times34561:',
             ),
             Text(
               '$_counter',
