2501695d62795b4e429a1b79b1a5f647dd72452c
yurakobernik <yurii.k@maxitechs.com>
2020-09-02 09:53:02 +0300

    added screens (home, login)

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_flutter_1/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
