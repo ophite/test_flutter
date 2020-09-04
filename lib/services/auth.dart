75ad59ba5cb5a53a5baec84dd8769bf5a4434e46
yurakobernik <yurii.k@maxitechs.com>
2020-09-04 12:56:28 +0300

    added reg user, settings, purchase, database service

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_first_flutter_1/models/user.dart';
+import 'package:my_first_flutter_1/models/user_settings.dart';
+import 'package:my_first_flutter_1/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -32,6 +34,36 @@ class AuthService {
       return await _auth.signOut();
     } catch (e) {
       print('signout error ${e.toString()}');
+      return null;
+    }
+  }
+
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // register with email and password
+  Future registerWithEmailAndPassword(
+      String email, String password, String name) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+
+      //create a new document with user uid
+      UserSettings userSettings = UserSettings(name: name, uid: user.uid);
+      await DatabaseService(uid: user.uid).updateUserSettings(userSettings);
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
       return null;
     }
   }
