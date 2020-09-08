81a39de457f9cd2efdccc36f25f4dd73767acd94
yurakobernik <yurii.k@maxitechs.com>
2020-09-08 18:44:32 +0300

    added google maps

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -1,8 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_first_flutter_1/services/auth.dart';
 
 class Map extends StatelessWidget {
+  Completer<GoogleMapController> _controller = Completer();
   final _auth = AuthService();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +35,23 @@ class Map extends StatelessWidget {
               label: Text('logout'))
         ],
       ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
