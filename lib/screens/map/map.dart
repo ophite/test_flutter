5505e18a3d4ed9e1d2e79077996af953d6fe95d8
yurakobernik <yurii.k@maxitechs.com>
2020-09-09 05:32:08 +0300

    added markers

--- lib/screens/map/map.dart
+++ lib/screens/map/map.dart
@@ -10,7 +10,7 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState2 extends State<MapPage> {
-  Completer<GoogleMapController> _controller = Completer();
+  // Completer<GoogleMapController> _controller = Completer();
   final _auth = AuthService();
 
   Widget build(BuildContext context) {
@@ -39,16 +39,62 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  GoogleMapController mapController;
+  static const _initialPosition = LatLng(12.97, 77.58);
+  LatLng _lastPosition = _initialPosition;
+  final Set<Marker> _markers = {};
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         GoogleMap(
           initialCameraPosition:
-              CameraPosition(target: LatLng(12.97, 77.58), zoom: 20.0),
+              CameraPosition(target: _initialPosition, zoom: 20.0),
+          onMapCreated: onCreate,
+          myLocationEnabled: true,
+          mapType: MapType.normal,
+          compassEnabled: true,
+          markers: _markers,
+          onCameraMove: _onCameraMove,
         ),
+        Positioned(
+          top: 40,
+          right: 10,
+          child: FloatingActionButton(
+            onPressed: _onAddMarkerPressed,
+            tooltip: "Add marker",
+            backgroundColor: Colors.black,
+            child: Icon(
+              Icons.add_location,
+              color: Colors.white,
+            ),
+          ),
+        )
       ],
     );
+  }
+
+  void onCreate(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    setState(() {
+      _lastPosition = position.target;
+    });
+  }
+
+  void _onAddMarkerPressed() {
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(_lastPosition.toString()),
+          position: _lastPosition,
+          infoWindow: InfoWindow(title: "Remember here", snippet: "good place"),
+          icon: BitmapDescriptor.defaultMarker));
+    });
   }
 }
 
