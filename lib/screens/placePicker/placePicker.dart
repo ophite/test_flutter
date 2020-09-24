5dbc6fe782b3ca7896b642cf94a059a9cdce477d
yurakobernik <yurii.k@maxitechs.com>
2020-09-24 11:26:28 +0300

    1st working commit for place picker

--- lib/screens/placePicker/placePicker.dart
+++ lib/screens/placePicker/placePicker.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter_1/screens/placePicker/models/pick_result.dart';
 import 'package:my_first_flutter_1/services/auth.dart';
+
+import 'others/place_picker.dart';
 
 class PlacePickerPage extends StatefulWidget {
   @override
@@ -29,7 +32,16 @@ class _PlacePickerPageState extends State<PlacePickerPage> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: RaisedButton(
-          child: Text('Place picker'),
+          child: PlacePicker(
+            apiKey:
+                "AIzaSyAGhEKlK0bWjqfDvPHE19uyd2kObvgXsyU", // Put YOUR OWN KEY here.
+            onPlacePicked: (PickResult result) {
+              print(result.adrAddress);
+              Navigator.of(context).pop();
+            },
+            // initialPosition: HomePage.kInitialPosition,
+            useCurrentLocation: true,
+          ),
         ),
       ),
     );
